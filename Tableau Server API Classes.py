8703d58ec8e18ae08fc4ecd0e7c355b0c8f2cff0
charapa123 <140089783+charapa123@users.noreply.github.com>
2024-08-22 11:18:18 +0100

    finding new rest URL with classes
    having the option to choose our endpoint but commented out is if we want all the endpoints

--- Tableau Server API Classes.py
+++ Tableau Server API Classes.py
@@ -2,11 +2,12 @@ import requests
 
 class credentials:
 
-    def __init__(self,PATName,PATSecret,site,base_url):
+    def __init__(self,PATName,PATSecret,site,base_url,endpoint):
         self.PATName = PATName
         self.PATSecret = PATSecret
         self.site = site
         self.base_url = base_url
+        self.endpoint = endpoint
 
     def user(self):
         body = f'''<tsRequest>
@@ -27,10 +28,52 @@ class credentials:
 #r = requests.post(url,body,headers=headers).text
         r = requests.post(url,body,headers=headers).json()
 
-        return r
+        token = r['credentials']['token']
+        site_id = r['credentials']['site']['id']
+
+        headers_get = {
+    'Accept': 'application/json',
+    'X-Tableau-Auth' : token
+}
+        # endpoints = ['workbooks','projects','groups','views']
+        
+        # endpoint_url = []
+        # for item in endpoints:
+        #     url = f'{self.base_url}{api}sites/{site_id}/{item}?includeUsageStatistics=true&fields=_all_'
+        #     endpoint_url.append(url)
+
+        endpoint_url = f'{self.base_url}{api}sites/{site_id}/{self.endpoint}?includeUsageStatistics=true&fields=_all_'
+
+
+        r = requests.get(endpoint_url,headers=headers_get).json()
+        page_number = int(r['pagination']['pageNumber'])
+        page_size = int(r['pagination']['pageSize'])
+        total_available = int(r['pagination']['totalAvailable'])
+
+        new_base_get = f'{self.base_url}{api}sites/{site_id}'
+
+
+        all_data = []
+        #this allows you to initialize the variables returned by the function to use seperately.
+        current_page = page_number
+
+        while total_available >= page_size*current_page:
+            get = requests.get(new_base_get+'/'+self.endpoint+'?includeUsageStatistics=true&fields=_all_&pageNumber='+str(current_page),headers=headers_get).json()
+            all_data.append(get)
+            print(new_base_get+'/'+self.endpoint+'?includeUsageStatistics=true&fields=_all_&pageNumber='+str(current_page))
+            current_page +=1  
+
+        return all_data
+    
     
 
-stuff = credentials('PATName','PATSecret','site','base_url')
+         
+
+
+    
+
+credentials('PATName', 'PATSecret', 'site', 'base_url','endpoint')
+
 
 print(stuff.user())
 
