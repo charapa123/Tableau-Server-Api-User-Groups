3671bf3347609d76a86592a1b6f1e321ff4d9c4b
charapa123 <140089783+charapa123@users.noreply.github.com>
2026-05-16 12:14:21 +0100

    Add SSL verification and refactor data handling
    Updated requests to include SSL certificate verification for API calls. Refactored data handling to use chosen_endpoint_data instead of all_data.

--- Credentials_Module.py
+++ Credentials_Module.py
@@ -1,6 +1,5 @@
 import requests
 import pandas as pd
-from datetime import datetime, timedelta
 
 #We first need to log in to our tableau server using the '/auth/signin' endpoint of the API
 #* Note
@@ -36,18 +35,18 @@ class Credentials:
         headers = {
     'Accept': 'application/json'
 }
+
         api = 'api/3.11/'
         auth = 'auth/signin'
         url = self.base_url+api+auth
 
 #.text shows the result in text
 #r = requests.post(url,body,headers=headers).text
-        r = requests.post(url,body,headers=headers).json()
+        r = requests.post(url,body,headers=headers,verify=r'C:\Users\PavlouC\Downloads\prod-uk-a.online.tableau.crt').json()
 
         token = r['credentials']['token']
         site_id = r['credentials']['site']['id']
         estimatedTimeToExpiration = r['credentials']['estimatedTimeToExpiration']
-        future_time = datetime.now() + timedelta(hours=int(estimatedTimeToExpiration.split(":")[0]), minutes=int(estimatedTimeToExpiration.split(":")[1]), seconds=int(estimatedTimeToExpiration.split(":")[2]))
         #If we've signed in, we get a connection token to use with future calls along with a site-id from the reponses (more xml). We rename the token 'X-Tableau-Auth'
 
         headers_get = {
@@ -57,7 +56,7 @@ class Credentials:
         return estimatedTimeToExpiration
     
     def chosen_endpoint(self):
-        global all_data
+        global chosen_endpoint_data
         global new_base_get
         global df_endpoint
         #We can attach this token as a header (along with Accept: application/json) and go to a different endpoint to get information about views,Workbooks,Groups,Projects on our Tableau Server
@@ -65,7 +64,7 @@ class Credentials:
         endpoint_url = f'{self.base_url}{api}sites/{site_id}/{self.endpoint}?includeUsageStatistics=true&fields=_all_'
 
 
-        r = requests.get(endpoint_url,headers=headers_get).json()
+        r = requests.get(endpoint_url,headers=headers_get,verify=r'C:\Users\PavlouC\Downloads\prod-uk-a.online.tableau.crt').json()
         page_number = int(r['pagination']['pageNumber'])
         page_size = int(r['pagination']['pageSize'])
         total_available = int(r['pagination']['totalAvailable'])
@@ -73,63 +72,82 @@ class Credentials:
         new_base_get = f'{self.base_url}{api}sites/{site_id}'
 
 
-        all_data = []
+        chosen_endpoint_data = []
         current_page = page_number
         #downloading all data available 
-        while total_available >= page_size*len(all_data):
-            get = requests.get(new_base_get+'/'+self.endpoint+'?includeUsageStatistics=true&fields=_all_&pageNumber='+str(current_page),headers=headers_get).json()
-            all_data.append(get)
+        while total_available >= page_size*len(chosen_endpoint_data):
+            get = requests.get(new_base_get+'/'+self.endpoint+'?includeUsageStatistics=true&fields=_all_&pageNumber='+str(current_page),headers=headers_get,verify=r'C:\Users\PavlouC\Downloads\prod-uk-a.online.tableau.crt').json()
+            chosen_endpoint_data.append(get)
             print(new_base_get+'/'+self.endpoint+'?includeUsageStatistics=true&fields=_all_&pageNumber='+str(current_page))
             current_page +=1 
             #parsing out as we are only interested in our endpoint response
-            data = []
-            for sublist in all_data:
-                data.append(pd.json_normalize(sublist[f'{self.endpoint}'][f"{self.endpoint.rstrip('s')}"]))
+            chosen_endpoint_clean_data = []
+            for sublist in chosen_endpoint_data:
+                chosen_endpoint_clean_data.append(pd.json_normalize(sublist[f'{self.endpoint}'][f"{self.endpoint.rstrip('s')}"]))
 
-        df_endpoint = pd.concat(data, ignore_index=True)
+        df_endpoint = pd.concat(chosen_endpoint_clean_data, ignore_index=True)
 
         return df_endpoint
     
     def permissions(self):
-        global stuff
+        global permissions_json
         #chosen_endpoint function downloads the initial data which provides us with an Id which can then be used in a second download to query permissions
         #Second download for permissions, need to get original fields to create permission URL and use id field from initial download. header Accept: application/json as XML is more inconsistent.
         df_endpoint['permissions'] = df_endpoint['id'].apply(lambda x: f'{new_base_get}/{self.endpoint}/{x}/permissions')
 
-        projects_permission_url = df_endpoint['permissions'].to_list()
+        endpoint_permission_url = df_endpoint['permissions'].to_list()
 
         # Initialize stuff as a list to store multiple permissions data
-        stuff = []
-        for download in projects_permission_url:
-            permissions_data = requests.get(download, headers=headers_get).json()
-            stuff.append(permissions_data)
+        permissions_json = []
+        for download in endpoint_permission_url:
+            permissions_data = requests.get(download, headers=headers_get,verify=r'C:\Users\PavlouC\Downloads\prod-uk-a.online.tableau.crt').json()
+            permissions_json.append(permissions_data)
 
-        return stuff
+
+
+        return permissions_json
 
     def permissions_group(self):
-        project_permissions_download = []
-        #Parse out Json and get group id information showing which groups have access. Then Summarise the important fields so group id (rename to which stream it is coming from so here workbooks so rename to workbook group id.
-        for permission_set in stuff:
-            if 'permissions' in permission_set:
-                data1 = permission_set['permissions']
+        # Parse out Json keeping the chosen endpoint id for each row so it can be joined back onto chosen endpoint function output and getting all permissions from granteeCapabilities list
+        # checking permissions is there for every downloaded endpoint
+        endpoint_permissions_download = []
+        for data_item in permissions_json:
+            permissions = data_item.get('permissions', {})
+            grantee_capabilities = permissions.get('granteeCapabilities', [])
+            for item in grantee_capabilities:
+                item[f"{self.endpoint.rstrip('s')}"] = permissions.get(f"{self.endpoint.rstrip('s')}", {}).get('id', 'Unknown')
+                endpoint_permissions_download.append(item)
+        # getting data into a dataframe
+        df = pd.json_normalize(endpoint_permissions_download)
+        # Row number added so this can be joined to the parsed out capabilities column (this df keeps the chosen endpoint ids to join back to chosen_endpoint data), this is due to the chosen endpoint id and other columns dropping when you json normalise a certain column
+        df['Row_Number'] = range(1, len(df) + 1)
+        # parsing out capabilities.capability column to columns as some will have read and write permissions enabled or more depending on endpoint
+        df_normalized = pd.json_normalize(df['capabilities.capability'])
+        # adding a row number to join back to df as the granularity changes after pivot
+        df_normalized['Row_Number'] = range(1, len(df) + 1)
+        # pivoting to get all columns from the split out capabilities column into 1 column reataining the row number
+        df_melt = df_normalized.melt(id_vars=['Row_Number'],
+                var_name="name",
+                value_name="mode")
+        # removing blank rows
+        df_melt =  df_melt[df_melt['mode'].notna()]
+        # adding a row number so we can join the normalised mode data back onto df_melt
+        df_melt['Row_Number1'] = range(1, len(df_melt) + 1)
+        # splits the mode (name and value) to columns 
+        df_mode_normalized = pd.json_normalize(df_melt['mode'])
+        # adds a row number to join back to df_melt
+        df_mode_normalized['Row_Number'] = range(1, len(df_mode_normalized) + 1)
 
-                endpoint_id = data1.get(f"{self.endpoint.rstrip('s')}", {}).get('id', None)
-                group_id = None
+        # this section is joining each dataframe to the one before to make sure granularity is correct and each id has the correct parsed out permissions
 
-                if 'granteeCapabilities' in data1 and data1['granteeCapabilities']:
-                    group_id = data1['granteeCapabilities'][0].get('group')
+        df_normal = pd.merge(df_mode_normalized,df_melt,'outer', right_on='Row_Number1', left_on='Row_Number')
 
-                if endpoint_id:
-                    ids = {
-                        'id': endpoint_id,
-                        'group_id': group_id['id'] if group_id is not None else 'N/A'
-                    }
-                    project_permissions_download.append(ids)
+        df_normal = df_normal[['name_x','mode_x','Row_Number_y']]
 
-        permissions_df = pd.DataFrame(project_permissions_download)
+        df_final = pd.merge(df,df_normal,'outer',left_on='Row_Number',right_on='Row_Number_y')
 
-        endpoint_df_with_permissions = pd.merge(df_endpoint,permissions_df,'outer', right_on='id',left_on='id')
+        df_final = df_final[[f"{self.endpoint.rstrip('s')}",'group.id','name_x','mode_x']].rename(columns={"name_x":"capabilities","mode_x":"mode"})
 
-        return endpoint_df_with_permissions
+        df_permissions_final = pd.merge(df_final,df_endpoint,'inner',left_on=f"{self.endpoint.rstrip('s')}",right_on='id')
 
-    #endpoint_df_with_permissions
+        return df_permissions_final
